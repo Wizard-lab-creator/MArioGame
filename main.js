af04826e87a61c51e9512d37a66e4f4ad447eea3
Wizard-lab-creator <80611597+Wizard-lab-creator@users.noreply.github.com>
2022-02-04 18:28:38 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCavas(1240, 336);
+	canvas.parent('canvas');
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+	video.parent('game_console');
+
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+	instializeInSetup(mario);
+}
+
+function draw() {
+	game();
+}
+function gotPoses(results){
+	if(results.length > 0){
+		console.log(results);
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+	}
+}
+
